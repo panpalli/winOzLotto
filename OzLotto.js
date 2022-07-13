56c3912be5a80a25dc31b79482e57ab3b9cb7dce
panpalli <memduhpanpalli@gmail.com>
2022-07-13 13:19:12 +1000

    Add files via upload

---
+++ OzLotto.js
@@ -0,0 +1,16 @@
+function createNumber(numberTop = 47){
+   return Math.ceil(Math.random() * numberTop) 
+}
+
+createNumber()
+
+var number1 = createNumber()
+var number2 = createNumber()
+var number2 = createNumber()
+var number3 = createNumber()
+var number4 = createNumber()
+var number5 = createNumber()
+var number6 = createNumber()
+var number7 = createNumber()
+
+console.log('Lott: ' + number1 + ' '+ number2 + ' '+ number3 + ' '+ number4 + ' '+ number5 + ' '+ number6 + ' '+ number7)
